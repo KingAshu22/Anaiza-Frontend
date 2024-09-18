a558e7c9f15bda2ebb3878a5c2be83d59aeb0147
KingAshu22 <ashishprasad9833@gmail.com>
2024-09-18 23:54:15 +0530

    updated ui of collections

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -12,29 +12,12 @@ const Collections = async () => {
         <p className="text-body-bold">No collections found</p>
       ) : (
         <div className="scroll-container flex">
-          <div className="scroll-content flex">
+          <div className="scroll-content flex ml-4">
             {collections.map((collection: CollectionType) => (
               <Link
                 href={`/collections/${collection._id}`}
                 key={collection._id}
-                className="flex-shrink-0 mx-2"
-              >
-                <Image
-                  src={collection.image}
-                  alt={collection.title}
-                  width={200}
-                  height={200}
-                  className="rounded-full cursor-pointer"
-                />
-                {/* <p className="text-center font-bold mt-1">{collection.title}</p> */}
-              </Link>
-            ))}
-            {/* Duplicating collections for seamless infinite scroll */}
-            {collections.map((collection: CollectionType) => (
-              <Link
-                href={`/collections/${collection._id}`}
-                key={`${collection._id}-duplicate`}
-                className="flex-shrink-0 mx-2"
+                className="flex-shrink-0 mr-2"
               >
                 <Image
                   src={collection.image}
